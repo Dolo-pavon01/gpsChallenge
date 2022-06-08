55eeb14cc0c372c66e44f216d990e38e16d2ba88
camilaayala01 <camilaayala01@gmail.com>
2022-06-08 19:56:18 -0300

    Cambios 8/6

---
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.modelo;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class Posicion {
+    private int x;
+    private int y;
+
+    public void trasladar(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public double distancia(Posicion unaPosicion) {
+        double p1 = pow(this.x - unaPosicion.x, 2);
+        double p2 = pow(this.y - unaPosicion.y, 2);
+        double p3 = sqrt(p1+p2);
+        return p3;
+    }
+
+
+}
