4e83677186d863191fa2048438cba4513dbf2aca
lorblak <kvasquez@fi.uba.ar>
2022-06-18 13:06:59 -0300

    refactor(modelo/ControlPolicial): se cambiaron algunas definciones; se agregaron pruebas

--- src/main/java/edu/fiuba/algo3/modelo/ControlPolicial.java
+++ src/main/java/edu/fiuba/algo3/modelo/ControlPolicial.java
@@ -3,11 +3,11 @@ package edu.fiuba.algo3.modelo;
 import java.util.Random;
 
 public class ControlPolicial implements IVisitor {
-  private static final int probabilidadMoto = 8;
-  private static final int probabilidadAuto = 5;
-  private static final int probabilidadAuto4x4 = 3;
-  private static final int limite = 10;
-  private static final int multa = 3;
+  private static final int PROBABILIDAD_MOTO = 8;
+  private static final int PROBABILIDAD_AUTO = 5;
+  private static final int PROBABILIDAD_AUTO4x4 = 3;
+  private static final int LIMITE = 10;
+  private static final int MULTA = 3;
   private Random randomizador;
 
   public ControlPolicial() {
@@ -18,29 +18,23 @@ public class ControlPolicial implements IVisitor {
     this.randomizador = randomizador;
   }
 
-  public int multar(int probabilidad) {
-    /* Random rand = new Random();
-    int parado = rand.nextInt(limite); // va entre 0 y 9
-    if (parado < probabilidad) return multa;
-    return 0;
-     */
-    int parado = this.randomizador.nextInt(limite);
-    if (parado < probabilidad) return multa;
-    return 0;
+  private int multar(int probabilidad) {
+    if (this.randomizador.nextInt(LIMITE) > probabilidad) return 0;
+    return MULTA;
   }
 
   @Override
   public int visit(Moto moto) {
-    return this.multar(probabilidadMoto);
+    return this.multar(PROBABILIDAD_MOTO);
   }
 
   @Override
   public int visit(Auto4x4 auto4x4) {
-    return this.multar(probabilidadAuto4x4);
+    return this.multar(PROBABILIDAD_AUTO4x4);
   }
 
   @Override
   public int visit(Auto auto) {
-    return this.multar(probabilidadAuto);
+    return this.multar(PROBABILIDAD_AUTO);
   }
 }
--- src/main/test/edu/fiuba/algo3/modelo/VisitorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/VisitorTest.java
@@ -3,44 +3,45 @@ package edu.fiuba.algo3.modelo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+// TODO: Faltan agregar pruebas de Pozo con Auto4x4
 public class VisitorTest {
   @Test
   public void test01PozoEsVisitadoPorMotoRetornaMulta3() {
     // arrange
-    int multaEsperada = 3;
     Moto motoMock = mock(Moto.class);
     Pozo pozo = new Pozo();
     // act
     int multa = pozo.visit(motoMock);
     // assert
-    assertEquals(multaEsperada, multa);
+    assertEquals(3, multa);
   }
 
   @Test
   public void test02PozoEsVisitadoPorAutoRetornaMulta3() {
     // arrange
-    int multaEsperada = 3;
     Auto autoMock = mock(Auto.class);
     Pozo pozo = new Pozo();
     // act
     int multa = pozo.visit(autoMock);
     // assert
-    assertEquals(multaEsperada, multa);
+    assertEquals(3, multa);
   }
 
   @Test
   public void test03PiqueteEsVisitadoPorMotoRetornaPenalizacion2() {
     // arrange
-    int multaEsperada = 2;
     Moto motoMock = mock(Moto.class);
     Piquete piquete = new Piquete();
     // act
     int multa = piquete.visit(motoMock);
     // assert
-    assertEquals(multaEsperada, multa);
+    assertEquals(2, multa);
   }
 
   @Test
@@ -67,5 +68,70 @@ public class VisitorTest {
         () -> {
           piquete.visit(autoMock);
         });
+  }
+
+  @Test
+  public void test06ControlPolicialEsVisitadoPorMotoYMotoNoEsMultado() {
+    // arrange
+    Moto motoMock = mock(Moto.class);
+    Random randomizadorMock = mock(Random.class);
+    when(randomizadorMock.nextInt(10)).thenReturn(9);
+    ControlPolicial controlPolicial = new ControlPolicial(randomizadorMock);
+    // act
+    int multa = controlPolicial.visit(motoMock);
+    // assert
+    assertEquals(0, multa);
+  }
+
+  @Test
+  public void test07ControlPolicialEsVisitadoPorMotoYMotoEsMultado() {
+    // arrange
+    Moto motoMock = mock(Moto.class);
+    Random randomizadorMock = mock(Random.class);
+    when(randomizadorMock.nextInt(10)).thenReturn(7);
+    ControlPolicial controlPolicial = new ControlPolicial(randomizadorMock);
+    // act
+    int multa = controlPolicial.visit(motoMock);
+    // assert
+    assertEquals(3, multa);
+  }
+
+  @Test
+  public void test08ControlPolicialEsVisitadoPorAutoYAutoNoEsMultado() {
+    // arrange
+    Auto autoMock = mock(Auto.class);
+    Random randomizadorMock = mock(Random.class);
+    when(randomizadorMock.nextInt(10)).thenReturn(6);
+    ControlPolicial controlPolicial = new ControlPolicial(randomizadorMock);
+    // act
+    int multa = controlPolicial.visit(autoMock);
+    // assert
+    assertEquals(0, multa);
+  }
+
+  @Test
+  public void test09ControlPolicialEsVisitadoPorAutoYAutoEsMultado() {
+    // arrange
+    Auto autoMock = mock(Auto.class);
+    Random randomizadorMock = mock(Random.class);
+    when(randomizadorMock.nextInt(10)).thenReturn(4);
+    ControlPolicial controlPolicial = new ControlPolicial(randomizadorMock);
+    // act
+    int multa = controlPolicial.visit(autoMock);
+    // assert
+    assertEquals(3, multa);
+  }
+
+  @Test
+  public void test10ControlPolicialEsVisitadoPorAuto4x4YAuto4x4NoEsMultado() {
+    // arrange
+    Auto4x4 autoMock = mock(Auto4x4.class);
+    Random randomizadorMock = mock(Random.class);
+    when(randomizadorMock.nextInt(10)).thenReturn(4);
+    ControlPolicial controlPolicial = new ControlPolicial(randomizadorMock);
+    // act
+    int multa = controlPolicial.visit(autoMock);
+    // assert
+    assertEquals(0, multa);
   }
 }
