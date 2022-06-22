e311a112c55e9b516b88ccce410c2ec5b5885d61
camilaayala01 <camilaayala01@gmail.com>
2022-06-22 19:25:33 -0300

    builders

--- src/main/java/edu/fiuba/algo3/AppCami.java
+++ src/main/java/edu/fiuba/algo3/AppCami.java
@@ -35,50 +35,17 @@ public class AppCami extends Application {
         Image image1 = new Image(input2);
         ImageView fondo=new ImageView(image1);
 
-        Rectangle rect=new Rectangle(); //instantiating Rectangle
-        rect.setX(0); //setting the X coordinate of upper left //corner of rectangle
-        rect.setY(0); //setting the Y coordinate of upper left //corner of rectangle
-        rect.setWidth(100); //setting the width of rectangle
-        rect.setHeight(100); // setting the height of rectangle
-        rect.setFill(Color.GREY);
-
-        Rectangle rect1=new Rectangle(); //instantiating Rectangle
-        rect1.setX(0); //setting the X coordinate of upper left //corner of rectangle
-        rect1.setY(150); //setting the Y coordinate of upper left //corner of rectangle
-        rect1.setWidth(100); //setting the width of rectangle
-        rect1.setHeight(100); // setting the height of rectangle
-        rect1.setFill(Color.GREY);
-
-        Rectangle rect2=new Rectangle(); //instantiating Rectangle
-        rect2.setX(150); //setting the X coordinate of upper left //corner of rectangle
-        rect2.setY(0); //setting the Y coordinate of upper left //corner of rectangle
-        rect2.setWidth(100); //setting the width of rectangle
-        rect2.setHeight(100); // setting the height of rectangle
-        rect2.setFill(Color.GREY);
-
-        Rectangle rect3=new Rectangle(); //instantiating Rectangle
-        rect3.setX(150); //setting the X coordinate of upper left //corner of rectangle
-        rect3.setY(150); //setting the Y coordinate of upper left //corner of rectangle
-        rect3.setWidth(100); //setting the width of rectangle
-        rect3.setHeight(100); // setting the height of rectangle
-        rect3.setFill(Color.GREY);
-
-
         Group group = new Group(); //creating Group
 
         group.getChildren().addAll(fondo);
         group.getChildren().addAll(img);
         group.getChildren().addAll(mover);
-        group.getChildren().addAll(rect,rect1,rect2,rect3);
 
 
         Translate translate = new Translate();
         translate.setX(20);
         translate.setY(0);
         translate.setZ(0);
-
-
-
 
 
         mover.setOnAction(new EventHandler<ActionEvent>() {
@@ -93,6 +60,8 @@ public class AppCami extends Application {
         primaryStage.setTitle("Aplicacion Cami");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
     public static void main (String[] args)
     {
