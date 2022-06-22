b9e3eb61eb8a50f648b7a2ce88e8de14ac3726f7
camilaayala01 <camilaayala01@gmail.com>
2022-06-22 20:24:58 -0300

    rotators

--- src/main/java/edu/fiuba/algo3/AppCami.java
+++ src/main/java/edu/fiuba/algo3/AppCami.java
@@ -1,28 +1,33 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.modelo.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 
 public class AppCami extends Application {
+    private Vehiculo vehiculo;
 
     public void start(Stage primaryStage) throws Exception {
 
-        Button mover=new Button("Mover auto");
-        mover.setLayoutX(50);
-        mover.setLayoutY(300);
 
         FileInputStream input=new FileInputStream("C:\\Users\\camil\\Documents\\GitHub\\gpsChallenge\\docs\\auto.png");
         Image image = new Image(input);
@@ -35,64 +40,133 @@ public class AppCami extends Application {
         Image image1 = new Image(input2);
         ImageView fondo=new ImageView(image1);
 
-        Rectangle rect=new Rectangle(); //instantiating Rectangle
-        rect.setX(0); //setting the X coordinate of upper left //corner of rectangle
-        rect.setY(0); //setting the Y coordinate of upper left //corner of rectangle
-        rect.setWidth(100); //setting the width of rectangle
-        rect.setHeight(100); // setting the height of rectangle
-        rect.setFill(Color.GREY);
-
-        Rectangle rect1=new Rectangle(); //instantiating Rectangle
-        rect1.setX(0); //setting the X coordinate of upper left //corner of rectangle
-        rect1.setY(150); //setting the Y coordinate of upper left //corner of rectangle
-        rect1.setWidth(100); //setting the width of rectangle
-        rect1.setHeight(100); // setting the height of rectangle
-        rect1.setFill(Color.GREY);
-
-        Rectangle rect2=new Rectangle(); //instantiating Rectangle
-        rect2.setX(150); //setting the X coordinate of upper left //corner of rectangle
-        rect2.setY(0); //setting the Y coordinate of upper left //corner of rectangle
-        rect2.setWidth(100); //setting the width of rectangle
-        rect2.setHeight(100); // setting the height of rectangle
-        rect2.setFill(Color.GREY);
-
-        Rectangle rect3=new Rectangle(); //instantiating Rectangle
-        rect3.setX(150); //setting the X coordinate of upper left //corner of rectangle
-        rect3.setY(150); //setting the Y coordinate of upper left //corner of rectangle
-        rect3.setWidth(100); //setting the width of rectangle
-        rect3.setHeight(100); // setting the height of rectangle
-        rect3.setFill(Color.GREY);
-
-
         Group group = new Group(); //creating Group
 
         group.getChildren().addAll(fondo);
         group.getChildren().addAll(img);
-        group.getChildren().addAll(mover);
-        group.getChildren().addAll(rect,rect1,rect2,rect3);
 
 
-        Translate translate = new Translate();
-        translate.setX(20);
-        translate.setY(0);
-        translate.setZ(0);
+        Translate arriba = new Translate();
+        arriba.setX(20);
+
+        Translate abajo = new Translate();
+        abajo.setX(-20);
+
+        Translate derecha = new Translate();
+        derecha.setX(20);
+
+        Translate izquierda = new Translate();
+        izquierda.setX(20);
+
+        Rotate haciaArriba = new Rotate();
+        haciaArriba.setAngle(90);
+
+        Rotate haciaAbajo = new Rotate();
+        haciaAbajo.setAngle(90);
+        haciaAbajo.setPivotX(20);
+        haciaAbajo.setPivotY(20);
+
+        TextField nombreInput = new TextField();
+        nombreInput.setPromptText("Ingresar nombre del jugador");
+
+        Label etiquetaVehiculos = new Label();
+        etiquetaVehiculos.setText("Elija su vehículo");
+
+        Button motoBtn = new Button();
+        motoBtn.setText("Moto");
+
+        Button autoBtn = new Button();
+        autoBtn.setText("Auto");
+
+        Button auto4x4Btn = new Button();
+        auto4x4Btn.setText("Auto 4x4");
+
+        HBox contenedorVehiculos = new HBox(motoBtn, autoBtn, auto4x4Btn);
+        contenedorVehiculos.setSpacing(10);
+
+        Label espacioVacio = new Label();
+
+        Button enviarBtn = new Button();
+        enviarBtn.setText("Enviar");
+
+        Button limpiarBtn = new Button();
+        limpiarBtn.setText("Limpiar");
+
+        HBox contenedorDatos = new HBox(enviarBtn, limpiarBtn);
+        contenedorDatos.setSpacing(10);
+        Button comenzar =new Button("COMIENZA A JUGAR");
+        VBox contenedorPrincipal =
+                new VBox(
+                        nombreInput, etiquetaVehiculos, contenedorVehiculos, espacioVacio, contenedorDatos,comenzar);
+        contenedorPrincipal.setSpacing(10);
+        contenedorPrincipal.setPadding(new Insets(20));
 
 
+        Scene inicio = new Scene(contenedorPrincipal, 300, 250);
+        primaryStage.setTitle("");
+        primaryStage.setScene(inicio);
+        primaryStage.show();
 
-
-
-        mover.setOnAction(new EventHandler<ActionEvent>() {
+        autoBtn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent arg0) {
-                img.getTransforms().addAll(translate);
+                 vehiculo = new Vehiculo(new Auto());
+            }
+        });
+        motoBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                vehiculo = new Vehiculo(new Moto());
+            }
+        });
+        auto4x4Btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                vehiculo = new Vehiculo(new Auto4x4());
+            }
+        });
+        Scene juego = new Scene(group,500,500, Color.BLACK);
+        comenzar.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                Gameplay gameplay = new Gameplay(new BuilderPruebas(20));
+                gameplay.iniciarJuego(vehiculo);
+                primaryStage.setScene(juego);
             }
         });
 
-        Scene scene = new Scene(group,500,500, Color.BLACK);
-        primaryStage.setTitle("Aplicacion Cami");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        juego.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode()== KeyCode.D) {
+                img.getTransforms().addAll(haciaArriba);
+                img.getTransforms().addAll(derecha);
+            }
+        });
+
+        juego.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode()== KeyCode.A) {
+                img.getTransforms().addAll(haciaAbajo);
+                img.getTransforms().addAll(izquierda);
+            }
+        });
+
+        juego.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode()== KeyCode.W) {
+
+                img.getTransforms().addAll(arriba);
+            }
+        });
+
+        juego.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode()== KeyCode.S) {
+
+                img.getTransforms().addAll(abajo);
+            }
+        });
+
     }
     public static void main (String[] args)
     {
