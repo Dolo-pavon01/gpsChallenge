b521ceea2a1e6b835a15c3166e9bea474413ae2a
camilaayala01 <camilaayala01@gmail.com>
2022-07-03 18:14:34 -0300

    problemas con limites del for

--- src/main/java/edu/fiuba/algo3/modelo/CreadorObstaculos.java
+++ src/main/java/edu/fiuba/algo3/modelo/CreadorObstaculos.java
@@ -6,8 +6,8 @@ public class CreadorObstaculos implements Creador {
 
   public HashMap crear(int largo, int ancho) {
     HashMap obstaculos = new HashMap<>(10, 70);
-    for (int i = 0; i < largo; i++) {
-      for (int j = 0; j < ancho; j++) {
+    for (int i = 0; i < ancho; i++) {
+      for (int j = 0; j < largo; j++) {
         Posicion p = Posicion.getPosicion(i, j);
         obstaculos.put(p, new ObstaculoNulo());
       }
